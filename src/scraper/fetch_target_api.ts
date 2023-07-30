994fb14e9352a47aaca35d246bf024611fe02f81
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-30 13:14:37 +0200

    replace kv in memory storage engine with deno kv engine

---
+++ src/scraper/fetch_target_api.ts
@@ -0,0 +1,20 @@
+import { curryCache, DenoKvStorageEngine } from "curry_cache";
+
+export const targetApiCache = new DenoKvStorageEngine({
+  name: "target-api-store",
+});
+
+export const fetchTargetAPI = curryCache(
+  async (targetUrl: string): Promise<any> => {
+    const url = new URL(targetUrl);
+    const apiUrl = url.origin + "/api" + url.pathname + url.search;
+
+    console.debug(
+      `fetchTargetApi function was called with url "${targetUrl}"! (Should only happen after cache clear or with different url!)`,
+    );
+    return await fetch(apiUrl).then((fetchResponse) => fetchResponse.json());
+  },
+  {
+    storageEngine: targetApiCache,
+  },
+);
