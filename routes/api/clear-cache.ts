dbccf140f06e9dbfb01b24d86c7688b31960a8dc
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-30 15:09:39 +0200

    add clear-cache hotfix

--- routes/api/clear-cache.ts
+++ routes/api/clear-cache.ts
@@ -1,5 +1,7 @@
 import { targetHtmlCache } from "@/src/scraper/fetch_target_html.ts";
 import { Handlers } from "$fresh/server.ts";
+import { showCache } from "@/src/aniflix_api.ts";
+import { targetApiCache } from "@/src/scraper/fetch_target_api.ts";
 
 // seems to not work => 405 - method not allowed error, despite sending a fetch with: fetch(`/api/clear-cache`, { method: "GET" })
 // export async function clearCache() {
@@ -9,7 +11,17 @@ import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
   async GET(_req, _ctx) {
+    // clearCache of showCache seems to not be working, so I'll clean DenoKv myself for now
+    await showCache.clearCache();
+    const kv = await Deno.openKv();
+
+    // Return all curry-cache entries & delete them!
+    const iter = kv.list<string>({ prefix: ["curry-cache"] });
+    for await (const res of iter) await kv.delete(res.key);
+
     await targetHtmlCache.clearCache();
+    await targetApiCache.clearCache();
+
     return new Response("Finished");
   },
 };
