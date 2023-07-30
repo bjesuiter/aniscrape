8ca17326b46f341cd07855490097c4d5e7f3096b
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-30 11:09:03 +0200

    finish url input

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,19 +8,23 @@ export default function Home(req: Request, ctx: RouteContext) {
   const count = useSignal(3);
   return (
     <Layout>
-      <section
+      <form
         id="search row"
         class="flex flex-row justify-center align-center p-6"
       >
         <label for="base-url-input" class="self-center">Series URL</label>
         <input
           id="base-url-input"
+          name="url"
           type="text"
           class="mx-4 p-1 text-slate-100 dark:text-slate-900 w-[500px]"
           value={searchParams.get("url") ?? ""}
         >
         </input>
-      </section>
+
+        <input type="submit" method="get" class="border-2 border-solid p-2">
+        </input>
+      </form>
     </Layout>
   );
 }
