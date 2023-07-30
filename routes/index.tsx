c4a2032592ea54ff4aaa925be700c15334a6ed47
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-30 10:57:08 +0200

    add basic input field for series url

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,28 +7,13 @@ export default function Home() {
   const count = useSignal(3);
   return (
     <Layout>
-      <p class="p-4">
-        Home
-      </p>
-      {
-        /* <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">Welcome to fresh</h1>
-          <p class="my-4">
-            Try updating this message in the
-            <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-          </p>
-          <Counter count={count} />
-        </div>
-      </div> */
-      }
+      <section
+        id="search row"
+        class="flex flex-row justify-center align-center p-6"
+      >
+        <label for="base-url-input">Series URL</label>
+        <input id="base-url-input" type="text" class="mx-4"></input>
+      </section>
     </Layout>
   );
 }
