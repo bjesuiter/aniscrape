0598d403d770993897d51e431fc631acc233ece0
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-30 10:44:14 +0200

    disable main menu for now

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ export function Layout(props: LayoutProps) {
   return (
     <div class="min-h-[100dvh] bg-slate-100 dark:bg-slate-700 text-slate-900 dark:text-slate-100">
       <header>
-        <MainNav></MainNav>
+        {/* <MainNav></MainNav> */}
       </header>
       <main>{props.children}</main>
     </div>
