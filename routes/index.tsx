b48faf681bb2733be8664fa5fd6e36d2cd1e4d0d
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-30 11:05:26 +0200

    add search params to index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,10 @@
-import { Head } from "$fresh/runtime.ts";
+import { RouteContext } from "$fresh/server.ts";
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
 import { Layout } from "../components/Layout.tsx";
 
-export default function Home() {
+export default function Home(req: Request, ctx: RouteContext) {
+  const searchParams = new URL(req.url).searchParams;
+
   const count = useSignal(3);
   return (
     <Layout>
@@ -16,6 +17,7 @@ export default function Home() {
           id="base-url-input"
           type="text"
           class="mx-4 p-1 text-slate-100 dark:text-slate-900 w-[500px]"
+          value={searchParams.get("url") ?? ""}
         >
         </input>
       </section>
