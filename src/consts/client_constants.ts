eda3931f0813d27514776dbde1e82dfa8efaaa4b
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-30 15:11:08 +0200

    add client constants file  for later use

---
+++ src/consts/client_constants.ts
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview This file contains all the constants for the client side.
+ * It is supposed to be save to import this file in the client side.
+ * Do NOT use imports in this file.
+ * Do NOT save server side constants in this file, as it will be exposed to the client.
+ * It can be used in server side as well.
+ */
