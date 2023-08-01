e51fb43336a80114f4eed47bf2a16aede4751818
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-08-01 22:51:23 +0200

    disable all WIP code

--- islands/aniflix-episode-tanstack.tsx
+++ islands/aniflix-episode-tanstack.tsx
@@ -1,13 +1,13 @@
 // Maybe we can use this guide (originally for nex.js) to correctly combine tanstack query with deno fresh islands!
 // https://tanstack.com/query/v5/docs/react/guides/ssr
 
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+// import {
+//   QueryClient,
+//   QueryClientProvider,
+//   useQuery,
+// } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 /**
  * This integration is WIP!!! Please use aniflix-episode-fetch for now!
@@ -17,8 +17,8 @@ export function AniflixEpisodeTanstack({ showName, season, episode }: {
   season: number;
   episode: number;
 }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-    </QueryClientProvider>
-  );
+  // return (
+  //   <QueryClientProvider client={queryClient}>
+  //   </QueryClientProvider>
+  // );
 }
