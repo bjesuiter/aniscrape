b2fe3205c5b52165bcdc19693ef35c8c975255ec
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-08-19 23:45:37 +0200

    add test for parallel fetches

--- research/parallel-fetches.ts
+++ research/parallel-fetches.ts
@@ -1,6 +1,16 @@
+import { fetchEpisodeUncached } from "@/src/aniflix_api.ts";
+import pMap from "p-map";
+
 const showName = "log-horizon";
 
 const episodeRequests = [];
+
+type EpisodeRequest = {
+  reqId: number;
+  showName: string;
+  seasonNumber: number;
+  episodeNumber: number;
+};
 
 for (let i = 0; i < 100; i++) {
   episodeRequests.push({
@@ -13,4 +23,21 @@ for (let i = 0; i < 100; i++) {
 
 // console.log(episodeRequests);
 
+// assemble async mapper for episode requests
+const mapper = async (episodeRequest: EpisodeRequest) => {
+  console.log(`fetching ${episodeRequest.reqId} ...`);
+  console.time(`fetch-${episodeRequest.reqId}`);
+  const episodeData = await fetchEpisodeUncached(episodeRequest);
+  console.timeEnd(`fetch-${episodeRequest.reqId}`);
+  return { ...episodeData, request: episodeRequest };
+};
 
+// split one rawEpisode into n UiEpisodes, depending on how many streaming services are available
+// rawEpisode === contains data for all streaming providers
+// UiEpisode === the episode on exactly one provider
+const rawEpisodes = await pMap(episodeRequests, mapper, {
+  concurrency: 1,
+});
+
+// Caution: overwrites console history due to big output!
+// console.log(rawEpisodes);
