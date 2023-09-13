d86f526fec1cb9faba5b0088962da4cc61a17193
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-09-13 10:35:35 +0200

    add debug island for sse stream

--- islands/sse-debug.tsx
+++ islands/sse-debug.tsx
@@ -1,13 +1,37 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
+import { signal } from "@preact/signals";
+import { SSEEvent } from "sse_codec";
 
 export function SSEDebug() {
   if (!IS_BROWSER) return <div></div>;
 
+  const messages = signal<SSEEvent[]>([]);
+
+  function addMessage(event: SSEEvent) {
+    messages.value = [...messages.value, event];
+  }
+
   const source = new EventSource("/streaming/custom");
 
-  source.onopen = () => console.log(`SSE Stream Opened!`);
-  source.onmessage = (msg) => console.log(`SSE Stream received message`, msg);
-  source.onerror = (err) => console.error(`SSE Stream had error`, err);
+  source.addEventListener("open", () => console.log(`SSE Stream Opened!`));
+  source.addEventListener("message", (msg) => {
+    console.log(`SSE Stream received message`, msg);
+    addMessage(msg);
+  });
+  source.addEventListener(
+    "error",
+    (err) => console.error(`SSE Stream had error`, err),
+  );
 
-  return <div></div>;
+  return (
+    <div>
+      {messages.value.map((msg: any) => (
+        <div>
+          <p>Event: {msg.eventName}</p>
+          <p>ID: {msg.id}</p>
+          <p>Data: {msg.data}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
