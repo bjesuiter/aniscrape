bacd02424987801f0c940a3442ea7ce02efbc3e4
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-09-16 22:55:55 +0200

    fix list rendering in sse-debug

--- islands/sse-debug.tsx
+++ islands/sse-debug.tsx
@@ -1,49 +1,66 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
-import { signal } from "@preact/signals";
+import { computed, signal, useComputed } from "@preact/signals";
 import { useEffect } from "preact/hooks";
 import { SSEEvent } from "sse_codec";
+
+let messages = signal<SSEEvent[]>([]);
+
+// Having this SSE Setup Code outside the component
+// fixes that eventsource might be recreated at re-rendering of this component!
+if (IS_BROWSER) {
+  messages = signal<SSEEvent[]>([]);
+
+  const addMessage = (msg: MessageEvent<any>) => {
+    const newArray = [...messages.value, {
+      id: msg.lastEventId,
+      data: msg.data,
+      eventName: msg.type,
+    }];
+    messages.value = newArray;
+  };
+
+  const source = new EventSource("/streaming/custom");
+
+  source.addEventListener("open", () => console.log(`SSE Stream Opened!`));
+
+  source.addEventListener("message", (msg) => {
+    console.log(`SSE Stream received message`, msg);
+    addMessage(msg);
+  });
+
+  // source.addEventListener("tick", (msg) => {
+  //   console.log(`SSE Stream received 'tick' event`, msg);
+  //   addMessage(msg);
+  // });
+
+  source.addEventListener(
+    "error",
+    (err) => console.error(`SSE Stream had error`, err),
+  );
+}
 
 export function SSEDebug() {
   if (!IS_BROWSER) return <div></div>;
 
-  const messages = signal<SSEEvent[]>([]);
+  const messageList = useComputed(() => {
+    // console.debug(messages.value);
+    return messages.value.map((msg: SSEEvent) => (
+      <div>
+        <p>Event: {msg?.eventName ?? "undefined"}</p>
+        <p>ID: {msg?.id ?? "undefined"}</p>
+        <p>Data: {msg?.data ?? "undefined"}</p>
+      </div>
+    ));
+  });
 
-  function addMessage(event: SSEEvent) {
-    messages.value = [...messages.value, event];
-  }
-
-  // This useEffect should only run when component is created
-  // This fixes that eventsource might be recreated at re-rendering of this component!
-  useEffect(() => {
-    const source = new EventSource("/streaming/custom");
-
-    source.addEventListener("open", () => console.log(`SSE Stream Opened!`));
-
-    source.addEventListener("message", (msg) => {
-      console.log(`SSE Stream received message`, msg);
-      addMessage(msg);
-    });
-
-    source.addEventListener("tick", (msg) => {
-      console.log(`SSE Stream received 'tick' event`, msg);
-      addMessage(msg);
-    });
-
-    source.addEventListener(
-      "error",
-      (err) => console.error(`SSE Stream had error`, err),
-    );
-  }, []);
+  const messageCount = computed(() => messages.value.length);
 
   return (
-    <div>
-      {messages.value.map((msg: any) => (
-        <div>
-          <p>Event: {msg.eventName}</p>
-          <p>ID: {msg.id}</p>
-          <p>Data: {msg.data}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <h1>Message Count: {messageCount}</h1>
+      <div>
+        {messageList.value}
+      </div>
+    </>
   );
 }
